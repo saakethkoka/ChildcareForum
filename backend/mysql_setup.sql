290a5a47341707e1d2afa697e26c0da81770bb84
saakethkoka <63078934+saakethkoka@users.noreply.github.com>
2022-03-01 18:42:22 -0600

    Initial commit

---
+++ backend/mysql_setup.sql
@@ -0,0 +1,18 @@
+-- create database db
+CREATE DATABASE db;
+
+-- use newly create database
+USE db;
+
+-- create table in db
+CREATE TABLE `db`.`test_table` (
+    `id` INT NOT NULL AUTO_INCREMENT, 
+    `value` VARCHAR(45), 
+    PRIMARY KEY (`id`), 
+    UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE
+);
+
+-- insert sample entry
+INSERT INTO `db`.`test_table` (`value`) VALUES ('Sample Value');
+
+
