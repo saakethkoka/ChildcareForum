6abe8927345172fadb59af676fe3045637b544ab
unknown <ijoseph@smu.edu>
2022-03-22 15:49:03 -0500

    login ui done

--- frontend/src/Accounts/Login.jsx
+++ frontend/src/Accounts/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Grid, Paper, Avatar } from '@mui/material/'
+import { Grid, Paper, Avatar, TextField, Button } from '@mui/material/'
 export class Login extends React.Component {
 
     state = {
@@ -16,14 +16,41 @@ export class Login extends React.Component {
     }*/
 
     render(){
-        const formStyle = {padding: 20, height: '70vh', width: 290, margin: "20px auto"}
+        const formStyle = {padding: 20, height: '50vh', width: 290, margin: "20px auto"}
+        const textStyle = {margin: "10px auto"}
         return(
             <Grid>
                 <Paper style = {formStyle}>
                     <Grid align = 'center'>
                         <h2>Sign in</h2>
                     </Grid>
-                
+                    <TextField 
+                        label = "Username" 
+                        placeholder = "Username"
+                        value={ this.state.username } 
+                        fullWidth required
+                        onChange={ e => this.setState({ username: e.target.value })}
+                        style = {textStyle}
+                    />
+                    <TextField 
+                        label = "Password" 
+                        placeholder = "Password"
+                        value={ this.state.password } 
+                        type = 'password'
+                        fullWidth required
+                        onChange={ e => this.setState({ password: e.target.value })}
+                        style = {textStyle}
+                    />
+                    <Button 
+                        type = "submit" 
+                        color = "primary" 
+                        variant='contained' 
+                        fullWidth
+                    >
+                        Sign in
+                    </Button>
+                    <span>Need an account? </span>
+                        <a href="/register">Register here</a>
                 </Paper>
             </Grid>
         )
