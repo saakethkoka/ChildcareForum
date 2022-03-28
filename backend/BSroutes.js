cd903e825ae37ea024424c3362dbf62f4a2c5a36
nsood1 <nbsood@mail.smu.edu>
2022-03-28 17:37:18 -0500

    add a get services

--- backend/BSroutes.js
+++ backend/BSroutes.js
@@ -1,14 +1,10 @@
-//Brad Sigety
+//Brad Sigety (Plus Nicole who slide in...)
 //March 22, 2022
 
 const express = require('express');
 
 const app = express();
 const port = 3000;
-
-
-
-
 
 //Request Query for all Disscussion Board 
 const connectToDatabase = require('./database-helpers');
@@ -31,7 +27,6 @@ module.exports = function BSroutes(app, logger) {
             response.status(500).json({message: err.message});
         }
     });
-
 
 //GET /discussionBoard?postTitle = ...
     app.get('/discussionBoard', async (request, response) => {
@@ -72,6 +67,22 @@ module.exports = function BSroutes(app, logger) {
             response.json(results);
         } catch (err) {
             console.error('There was an error in GET /discussionBoard', err);
+            response.status(500).json({message: err.message});
+        }
+    });
+
+//Nicole Sood: 
+
+    //Get Services, this will list out
+    app.get('/services', async(request, response) => {
+        try {
+            console.log('Initiating getServices...');
+            const{DBQuery, disconnect}  = await connectToDatabase();
+            const results = await DBQuery('SELECT * FROM addService');
+            disconnect();
+            response.json(results);
+        } catch (err) {
+            console.error('There was an error in GET /services', err);
             response.status(500).json({message: err.message});
         }
     });
