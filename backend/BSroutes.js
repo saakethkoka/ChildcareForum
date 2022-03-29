c7a59094ac81c2e080dbc26487dc65549fc35d57
nsood1 <nbsood@mail.smu.edu>
2022-03-28 20:41:59 -0500

    get review based off of serviceID

--- backend/BSroutes.js
+++ backend/BSroutes.js
@@ -72,15 +72,51 @@ module.exports = function BSroutes(app, logger) {
     });
 
 //Nicole Sood: 
-
-    //Get Services, this will list out
+    //Get Services, this will list out the services provided by a specific user profile. 
     app.get('/services', async(request, response) => {
         try {
             console.log('Initiating getServices...');
             const{DBQuery, disconnect}  = await connectToDatabase();
-            const results = await DBQuery('SELECT * FROM addService');
+            const userID = request.query.userID;
+            let results;
+            if(userID){
+                const results = await DBQuery('SELECT * FROM addService WHERE f_userID = ?', [userID]);
+                response.json(results);
+            }
+
+            else{
+                const results = await DBQuery('SELECT * FROM addService');
+                response.json(results);
+            }
             disconnect();
-            response.json(results);
+            //response.json(results);
+
+        } catch (err) {
+            console.error('There was an error in GET /services', err);
+            response.status(500).json({message: err.message});
+        }
+    });
+
+
+    //
+    app.get('/review', async(request, response) => {
+        try {
+            console.log('Initiating getReview...');
+            const{DBQuery, disconnect}  = await connectToDatabase();
+            const serviceID = request.query.serviceID;
+            let results;
+            if(serviceID){
+                const results = await DBQuery('SELECT review FROM reviewService where f_serviceID = ?', [serviceID]);
+                response.json(results);
+            }
+
+            else{
+                const results = await DBQuery('SELECT * FROM reviewService');
+                response.json(results);
+            }
+            disconnect();
+            //response.json(results);
+
         } catch (err) {
             console.error('There was an error in GET /services', err);
             response.status(500).json({message: err.message});
