516bb1ba6c37bbff153ddd494816e4d817a04b26
unknown <ijoseph@smu.edu>
2022-04-19 17:45:25 -0500

    changes

--- frontend/src/Routing.jsx
+++ frontend/src/Routing.jsx
@@ -19,6 +19,9 @@ import { ProfileDislikedPosts } from "./Profiles/ProfileComponents/Views/Profile
 
 
 
+import ProfileView from "./Profiles/ProfileComponents/Views/ViewOtherUser/OtherProfileView";
+
+
 export default function Routing(){
 	const checkLogin = () =>{
 		console.log()
