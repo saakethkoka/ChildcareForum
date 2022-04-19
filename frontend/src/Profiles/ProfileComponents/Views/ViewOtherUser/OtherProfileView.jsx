b37c88e37ce31edcce9993f6d95d089cd15b1f47
unknown <ijoseph@smu.edu>
2022-04-19 17:45:25 -0500

    changes

--- frontend/src/Profiles/ProfileComponents/Views/ViewOtherUser/OtherProfileView.jsx
+++ frontend/src/Profiles/ProfileComponents/Views/ViewOtherUser/OtherProfileView.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { useState, useEffect } from "react";
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { TextField, Container, Paper, Card, Stack, Grid, InputAdornment, FormControl, OutlinedInput, FormHelperText, InputLabel, IconButton, Button, Chip, Alert, Typography, Fab } from "@mui/material";
 =======
 import UserNavBar from "../UserNavBar"
 import { TextField, Container, Paper, Card, Stack, Grid, InputAdornment, FormControl, OutlinedInput, FormHelperText, InputLabel, IconButton, Button, Chip, Alert } from "@mui/material";
 >>>>>>> 6a7d8e2 (adding ui for view other profile)
+=======
+import { TextField, Container, Paper, Card, Stack, Grid, InputAdornment, FormControl, OutlinedInput, FormHelperText, InputLabel, IconButton, Button, Chip, Alert, Typography, Fab } from "@mui/material";
+>>>>>>> a1f1bed (changes)
 import BuildIcon from '@mui/icons-material/Build';
 import { Avatar } from "@mui/material";
 import { spacing } from '@mui/system';
@@ -14,9 +18,13 @@ import CloseIcon from '@mui/icons-material/Close';
 import VerifiedIcon from '@mui/icons-material/Verified';
 import GavelRoundedIcon from '@mui/icons-material/GavelRounded';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import OtherProfileNavbar from "./OtherUserNavBar";
 =======
 >>>>>>> 6a7d8e2 (adding ui for view other profile)
+=======
+import OtherProfileNavbar from "./OtherUserNavBar";
+>>>>>>> a1f1bed (changes)
 // react-bootstrap components
 
 
@@ -27,6 +35,7 @@ import OtherProfileNavbar from "./OtherUserNavBar";
 function ProfileView() {
   const textStyle = {margin: "10px auto"}
 <<<<<<< HEAD
+<<<<<<< HEAD
   const formStyle = {padding: 20, height: '50vh', width: '50vh', margin: "20px auto"}
 
   const testUser = [{id: 1, first_name: "John", last_name: "Doe", email: "test@gmail.com", password: "test123", username: "johndoe", verificationStatus:true, banStatus: false}]
@@ -35,11 +44,17 @@ function ProfileView() {
 
   const testUser = [{id: 1, first_name: "John", last_name: "Doe", email: "test@gmail.com", password: "test123", username: "johndoe"}]
 >>>>>>> 6a7d8e2 (adding ui for view other profile)
+=======
+  const formStyle = {padding: 20, height: '50vh', width: '50vh', margin: "20px auto"}
+
+  const testUser = [{id: 1, first_name: "John", last_name: "Doe", email: "test@gmail.com", password: "test123", username: "johndoe", verificationStatus:true, banStatus: false}]
+>>>>>>> a1f1bed (changes)
 
   const [first_Name, setFirstName] = useState("");
   const [last_Name, setLastName] = useState("");
   const [username, setUsername] = useState("");
   const [verificationStatus, setVerificationStatus] = useState("");
+<<<<<<< HEAD
 <<<<<<< HEAD
   const [banStatus, setBanStatus]  = useState(false)
 =======
@@ -48,12 +63,16 @@ function ProfileView() {
   const [matchPasswords, setMatchPasswords] = useState(true);
 
 >>>>>>> 6a7d8e2 (adding ui for view other profile)
+=======
+  const [banStatus, setBanStatus]  = useState(false)
+>>>>>>> a1f1bed (changes)
 
 
   useEffect(() => {
           setFirstName(testUser[0].first_name)
           setLastName(testUser[0].last_name)
           setUsername(testUser[0].username)
+<<<<<<< HEAD
 <<<<<<< HEAD
           setBanStatus(testUser[0].banStatus)
           setVerificationStatus("verified")
@@ -73,18 +92,35 @@ function ProfileView() {
     <>
       <OtherProfileNavbar/>
 =======
+=======
+          setBanStatus(testUser[0].banStatus)
+>>>>>>> a1f1bed (changes)
           setVerificationStatus("verified")
-          
+    
   }, []);
+
+  function toggleUserBan(){
+    if(banStatus == true){
+      setBanStatus(false)
+    }
+    else{
+      setBanStatus(true)
+    }
+  }
 
   return (
     <>
+<<<<<<< HEAD
       <UserNavBar/>
 >>>>>>> 6a7d8e2 (adding ui for view other profile)
+=======
+      <OtherProfileNavbar/>
+>>>>>>> a1f1bed (changes)
       
       <Grid align = 'center'>
         <Paper style = {formStyle} elevation = {12}>
           <Grid align='center'>
+<<<<<<< HEAD
 <<<<<<< HEAD
               <Typography variant="h5" component="div">
                         {username} {verificationStatus && <VerifiedIcon color = "primary"/>}
@@ -132,41 +168,56 @@ function ProfileView() {
 =======
             <Avatar ><BuildIcon/></Avatar>
             <h3>Username</h3>
+=======
+              <Typography variant="h5" component="div">
+                        {username} {verificationStatus && <VerifiedIcon color = "primary"/>}
+              </Typography>
+>>>>>>> a1f1bed (changes)
           </Grid>
         <div>
 
-          <FormControl sx={{ m: 2, width: '25ch' }} variant="outlined">
-            <InputLabel >Username</InputLabel>
-            <OutlinedInput
-              type="text"
-              value={username}
-              label="Username"
-            />
-          </FormControl>
+
           
-          <FormControl sx={{ m: 2, width: '25ch' }} variant="outlined">
-            <InputLabel htmlFor="outlined-adornment-password">First Name</InputLabel>
-            <OutlinedInput
-              id="outlined-adornment-password"
-              type="text"
-              value={first_Name}
-              label="First Name"
-            />
-          </FormControl>
+          <TextField sx={{ m: 2, width: '25ch' }} variant="outlined"
+            id="outlined-read-only-input"
+            label="First Name"
+            value={first_Name}
+            InputProps={{
+              readOnly: true,
+            }}
+          >
+          </TextField>
 
 
-          <FormControl sx={{ m: 2, width: '25ch' }} variant="outlined">
-            <InputLabel htmlFor="outlined-adornment-password">Last Name</InputLabel>
-            <OutlinedInput
-              id="outlined-adornment-password"
-              type="text"
-              value={last_Name}
-              label="Last Name"
-            />
-          </FormControl>
+          <TextField sx={{ m: 2, width: '25ch' }} variant="outlined"
+            id="outlined-read-only-input"
+            label="Last Name"
+            value={last_Name}
+            InputProps={{
+              readOnly: true,
+            }}
+          >
+          </TextField>
+          
+          
+            
           <Grid align='center'>
+<<<<<<< HEAD
 
 >>>>>>> 6a7d8e2 (adding ui for view other profile)
+=======
+            {!banStatus && <Fab color="error"
+                //onClick={e => unBan(e)}
+                aria-label="Ban"
+                onClick={e=>toggleUserBan(e)}>
+              <GavelRoundedIcon/>
+            </Fab>}
+            {banStatus && <item>
+              <Button variant="contained"  color = "error" onClick={e=>toggleUserBan(e)}>
+                Banned
+              </Button>
+              </item>}
+>>>>>>> a1f1bed (changes)
           </Grid>
 
         </div>
--- frontend/src/Routing.jsx
+++ frontend/src/Routing.jsx
@@ -7,6 +7,9 @@ import PostBoard from "./PostBoard/PostBoard";
 import ProfileView from "./Profiles/ProfileComponents/Views/ViewOtherUser/OtherProfileView";
 
 
+import ProfileView from "./Profiles/ProfileComponents/Views/ViewOtherUser/OtherProfileView";
+
+
 export default function Routing(){
 	const checkLogin = () =>{
 		console.log()
@@ -19,6 +22,18 @@ export default function Routing(){
     return(
         <Router>
 			<Routes>
+<<<<<<< HEAD
+=======
+				<Route path="/user/profile/:userId" exact element={<ProfileView />} />
+				<Route path="/user/posts/:userId" exact element={<OtherProfilePosts />} />
+                <Route path="/accounts/userProfile" exact element={<UserProfile />} />
+				<Route path="/accounts/userPosts" exact element={<ProfilePosts />} />
+				<Route path="/accounts/ban" exact element={<ProfileBanned />} />
+				
+                
+                
+				
+>>>>>>> a1f1bed (changes)
 
 				<Route path="/" element={checkLogin()} />
 				<Route path="/posts" exact element={<PostBoard />} />
