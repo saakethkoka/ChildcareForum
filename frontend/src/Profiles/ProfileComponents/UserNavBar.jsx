0d87d8b441c8ab81bc6256091b3c92dbcdaef8d9
unknown <ijoseph@smu.edu>
2022-04-20 06:35:06 -0500

    fixed

---
+++ frontend/src/Profiles/ProfileComponents/UserNavBar.jsx
@@ -0,0 +1,24 @@
+import { AppBar, Toolbar, IconButton, Typography, Stack, Button } from '@mui/material'
+
+const ProfileNavbar = (props) => {
+    console.log(props)
+    return (
+        <AppBar position = 'static'>
+            <Toolbar>
+                <Typography variant = 'h6' component='div' sx={{ flexGrow: 1}}>
+                    <Button color = 'inherit'>Home</Button>
+                </Typography>
+                <Stack direction='row' spacing={2}>
+                    {props.auth && <Button color = 'inherit'>Verification Requests</Button>}
+                    <Button color = 'inherit'>Post History</Button>
+                    <Button color = 'inherit'>Comments</Button>
+                    <Button color = 'inherit'>Like/Dislike</Button>
+                    <Button color = 'inherit'>Saved Posts</Button>
+                    {props.auth && <Button color = 'inherit'>Banned Users</Button>}
+                </Stack>
+            </Toolbar>
+        </AppBar>
+    )
+}
+
+export default ProfileNavbar
