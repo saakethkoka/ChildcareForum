a6901d4767a829af47db075d2f4be2457ccda739
unknown <ijoseph@smu.edu>
2022-04-20 06:37:46 -0500

    Resolved

---
+++ frontend/src/Profiles/ProfileComponents/Common/PostBoard/PostList/Post/CommentList/CommentList.jsx
@@ -0,0 +1,129 @@
+import React, { Fragment, useEffect} from 'react';
+import {Dialog, TextField} from "@mui/material";
+import Comment from './Comment/Comment';
+import Grid from "@mui/material/Grid/Grid";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+const sample_comments = [
+    {author:"john", content: "This is the first comment", userVote: 1, votes: 0, id: 1},
+    {author:"bob", content: "This is the second comment", userVote: 0, votes: 0, id: 2},
+    {author:"bob", content: "This is the third comment", userVote: -1, votes: 0, id: 3},
+    {author:"bob", content: "This is the fourth comment", userVote: 0, votes: 0, id: 4},
+    {author:"bob", content: "This is the fifth comment", userVote: 0, votes: 0, id: 5},
+    {author:"bob", content: "This is the sixth comment", userVote: 0, votes: 0, id: 6},
+    {author:"bob", content: "This isThis is the first commentThis is the first commentThis is the first commentThis is the first commentThis is the first commentThis is the first commentThis is the first commentThis is the first commentThis is the first commentThis is the first commentThis is the first commentThis is the first commentThis is the first commentThis is the first commentThis is the first commentThis is the first commentThis is the first commentThis is the first commentThis is the first commentThis is the first commentThis is the first commentThis is the first commentThis is the first commentThis is the first commentThis is the first commentThis is the first commentThis is the first commentThis is the first commentThis is the first commentThis is the first commentThis is the first commentThis is the first commentThis is the first commentThis is the first commentThis is the first commentThis is the first commentThis is the first commentThis is the first commentThis is the first commentThis is the first commentThis is the first comment the seventh comment", userVote: 0, votes: 0, id: 7},
+    {author:"bob", content: "This is the eighth comment", userVote: 0, votes: 0, id: 8},
+    {author:"bob", content: "This is the ninth comment", userVote: 0, votes: 0, id: 9},
+    {author:"bob", content: "This is the tenth comment", userVote: 0, votes: 0, id: 10},
+]
+
+export default function CommentList(props){
+
+    let [comment, setComment] = React.useState("");
+    let [comments, setComments] = React.useState(sample_comments);
+
+
+    const onCancel = () => {
+        // TODO: clear form maybe
+        props.onClose();
+    }
+
+    const onCommentSubmit = () => {
+        console.log(comment);
+        setComment("");
+        let new_comments = comments.push({
+            author: "bob",
+            content: comment,
+            userVote: 0,
+            votes: 0,
+            id: comments.length + 1
+        });
+    }
+
+
+    const onUpvote = (id) => {
+        let new_comments = comments.map(comment => {
+            if(comment.id === id){
+                if(comment.userVote === -1) {
+                    comment.userVote = 1;
+                    comment.votes += 2;
+                } else if(comment.userVote === 0) {
+                    comment.userVote = 1;
+                    comment.votes += 1;
+                }
+                else{
+                    comment.userVote = 0;
+                    comment.votes -= 1;
+                }
+            }
+            return comment;
+        });
+        setComments(new_comments);
+    }
+
+    const onDownvote = (id) => {
+        let new_comments = comments.map(comment => {
+            if(comment.id === id){
+                if(comment.userVote === 1) {
+                    comment.userVote = -1;
+                    comment.votes -= 2;
+                } else if(comment.userVote === 0) {
+                    comment.userVote = -1;
+                    comment.votes -= 1;
+                }
+                else{
+                    comment.userVote = 0;
+                    comment.votes += 1;
+                }
+            }
+            return comment;
+        });
+        setComments(new_comments);
+    }
+
+    return(
+        <Dialog open={props.open}
+                onClose={onCancel}
+                aria-labelledby="alert-dialog-title"
+                fullWidth
+                scroll='body'
+                sx={{overflowY: "scroll"}}
+                aria-describedby="alert-dialog-description">
+
+            <Typography variant="h5"
+                        sx={{margin: "1rem"}}
+                        component="div">
+                Comments
+            </Typography>
+            {sample_comments.map(comment =>
+                <Comment key={comment.id}
+                         onUpvote={onUpvote}
+                         onDownvote={onDownvote}
+                         comment={comment}/>
+            )}
+            <form noValidate autoComplete="off">
+                <TextField id="comment"
+                           label="Comment"
+                           variant="outlined"
+                           multiline
+                           value={comment}
+                           onChange={(e) => setComment(e.target.value)}
+                           fullWidth
+                           minRows={4}
+                           className="mb-16"
+                />
+                <Button variant="contained"
+                        color="primary"
+                        type="button"
+                        sx={{margin: "1rem 0 0 0"}}
+                        onClick={onCommentSubmit}
+                        fullWidth>
+                    Post
+                </Button>
+            </form>
+
+
+        </Dialog>
+    )
+}
