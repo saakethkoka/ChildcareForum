71fbaec86f34b579c01414178f61bb43ffa24305
swward <swward@smu.edu>
2022-04-21 15:02:20 -0500

    added GET /discussionBoard/[userID]

--- backend/postRoutes.js
+++ backend/postRoutes.js
@@ -94,6 +94,92 @@ router.get('/', async (req, res, next) => {
     next();
 })
 
+router.get('/:userID', async (req, res, next) => {
+    try {
+        console.log('Initiating GET /discussionBoard/[userID] request');
+        const {DBQuery, disconnect} = await connectToDatabase();
+        const results = await DBQuery('SELECT * FROM discussionBoard WHERE f_userID = ' + req.params.userID);
+        
+        const postRaw = JSON.parse(JSON.stringify(results));
+        let formattedPosts = [];
+        for (const row in postRaw) {
+            const statusQuery = 'SELECT isVerified, isBanned FROM statusTable WHERE userID = ' + postRaw[row].f_userID;
+            const statusResults = await DBQuery(statusQuery);
+            const statusRaw = JSON.parse(JSON.stringify(statusResults));
+
+            const userQuery = 'SELECT username FROM userLogin WHERE userID = ' + postRaw[row].f_userID;
+            const userResults = await DBQuery(userQuery);
+            const userRaw = JSON.parse(JSON.stringify(userResults));
+
+            let userVote;
+            if (typeof req.query.curruserID == 'undefined')
+                userVote = 0;
+            else {
+                const voteValueRaw = await DBQuery('SELECT value FROM postVotes WHERE f_postID = ' + postRaw[row].postID + ' AND f_userID = ' + req.query.curruserID);
+                const voteValueObj = JSON.parse(JSON.stringify(voteValueRaw));
+                if (typeof voteValueObj[0] == 'undefined')
+                    userVote = 0;
+                else
+                    userVote = voteValueObj[0].value;
+            }
+
+            let votes;
+            const voteCountRaw = await DBQuery('SELECT SUM(value) AS total FROM postVotes WHERE f_postID = ' + postRaw[row].postID);
+            const voteCountObj = JSON.parse(JSON.stringify(voteCountRaw))[0];
+            if (voteCountObj.total === null)
+                votes = 0;
+            else
+                votes = voteCountObj.total;
+
+            let verified;
+            if(statusRaw[0]){
+                verified = (statusRaw[0].isVerified !== 0)
+            }
+            else{
+                verified = false;
+            }
+
+            let restricted;
+            if(statusRaw[0]){
+                restricted = (statusRaw[0].isRestricted !== 0)
+            }
+            else{
+                restricted = false;
+            }
+
+            let userBanned;
+            if(statusRaw[0]){
+                userBanned = (statusRaw[0].isBanned !== 0)
+            }
+            else{
+                userBanned = false;
+            }
+
+            formattedPosts.push({
+                postTitle: postRaw[row].postTitle,
+                postID: postRaw[row].postID,
+                userID: postRaw[row].f_userID,
+                votes: votes,
+                userVote: userVote,
+                verified,
+                date: postRaw[row].date,
+                restricted,
+                username: userRaw[0].username,
+                userBanned,
+                postEntry: postRaw[row].postEntry
+            });
+        }
+
+        disconnect();
+        res.json(formattedPosts);
+    } catch (err) {
+        console.error('Problem retrieving discussion board posts by author ID', err);
+        res.status(500).json({message: err.toString()});
+    }
+
+    next();
+})
+
 //returns discussionBoard posts in order of votes
 router.get('/voteorder', async (req, res, next) => {
     try {
