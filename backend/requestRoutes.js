c36087f7f0454175b59b3973fccfc4766d8d7c28
swward <swward@smu.edu>
2022-04-21 18:16:17 -0500

    finished remaining request routes

--- backend/requestRoutes.js
+++ backend/requestRoutes.js
@@ -15,7 +15,30 @@ router.get('/pending', async (req, res, next) => {
                             + ' WHERE hasRequested = 1';
         const results = await DBQuery(queryString);
         disconnect();
-        res.json(results);
+        res.status(200).json(results);
+    } catch (err) {
+        console.error('Issue retrieving pending requests', err);
+        res.status(500).json({message: err.toString()});
+    }
+
+    next();
+})
+
+router.get('/status/:userID', async(req, res, next) => {
+    try {
+        console.log('Initiating GET /requests/status/[userID] request');
+        const {DBQuery, disconnect} = await connectToDatabase();
+
+        //check if user has a row in statusTable, adding if not
+        const checkRow = await DBQuery('SELECT * FROM statusTable WHERE userID = ' + req.params.userID);
+        if (JSON.stringify(checkRow) === '{}')
+            newRow = await DBQuery('INSERT INTO statusTable (userID) VALUES (' + req.params.userID + ')');
+
+        const queryString = 'SELECT IFNULL(hasRequested, 0) AS status FROM statusTable WHERE userID = ' + req.params.userID;
+        const results = await DBQuery(queryString);
+        const resultsObj = JSON.parse(JSON.stringify(results));
+        disconnect();
+        res.status(200).json({status: (resultsObj[0].status == 1)});
     } catch (err) {
         console.error('Issue retrieving pending requests', err);
         res.status(500).json({message: err.toString()});
@@ -27,6 +50,13 @@ router.get('/pending', async (req, res, next) => {
 router.put('/approve/:userID', async (req, res, next) => {
     try {
         console.log('Initiating PUT /requests/approve/[userID] request');
+        const {DBQuery, disconnect} = await connectToDatabase();
+        const queryString = 'UPDATE statusTable'
+                            + ' SET isVerified = 1 AND hasRequested = 0'
+                            + ' WHERE userID = ' + req.params.userID;
+        const results = await DBQuery(queryString);
+        disconnect();
+        res.status(200).json(results);
     } catch (err) {
         console.error('Issue approving verification request', err);
         res.status(500).json({message: err.toString()});
@@ -38,6 +68,13 @@ router.put('/approve/:userID', async (req, res, next) => {
 router.put('/reject/:userID', async (req, res, next) => {
     try {
         console.log('Initiating PUT /requests/reject/[userID] request');
+        const {DBQuery, disconnect} = await connectToDatabase();
+        const queryString = 'UPDATE statusTable'
+                            + ' SET isVerified = 0 AND hasRequested = 0'
+                            + ' WHERE userID = ' + req.params.userID;
+        const results = await DBQuery(queryString);
+        disconnect();
+        res.status(200).json(results);
     } catch (err) {
         console.error('Issue rejecting verification request', err);
         res.status(500).json({message: err.toString()});
@@ -46,9 +83,22 @@ router.put('/reject/:userID', async (req, res, next) => {
     next();
 })
 
-router.put('/newpending', async (req, res, next) => {
+router.put('/makepending/:userID', async (req, res, next) => {
     try {
-        console.log('Initiating PUT /requests/newpending request');
+        console.log('Initiating PUT /requests/makepending/[userID] request');
+        const {DBQuery, disconnect} = await connectToDatabase();
+        
+        //check if user has a row in statusTable, adding if not
+        const checkRow = await DBQuery('SELECT * FROM statusTable WHERE userID = ' + req.params.userID);
+        if (JSON.stringify(checkRow) === '{}')
+            newRow = await DBQuery('INSERT INTO statusTable (userID) VALUES (' + req.params.userID + ')');
+
+        const queryString = 'UPDATE statusTable'
+                            + ' SET hasRequested = 1'
+                            + ' WHERE userID = ' + req.params.userID;
+        const results = await DBQuery(queryString);
+        disconnect();
+        res.status(200).json(results);
     } catch (err) {
         console.error('Issue adding user to pending requests', err);
         res.status(500).json({message: err.toString()});
@@ -57,9 +107,16 @@ router.put('/newpending', async (req, res, next) => {
     next();
 })
 
-router.put('/removepending', async (req, res, next) => {
+router.put('/removepending/:userID', async (req, res, next) => {
     try {
-        console.log('Initiating PUT /requests/removepending request');
+        console.log('Initiating PUT /requests/removepending/[userID] request');
+        const {DBQuery, disconnect} = await connectToDatabase();
+        const queryString = 'UPDATE statusTable'
+                            + ' SET hasRequested = 0'
+                            + ' WHERE userID = ' + req.params.userID;
+        const results = await DBQuery(queryString);
+        disconnect();
+        res.status(200).json(results);
     } catch (err) {
         console.error('Issue removing user pending request', err);
         res.status(500).json({message: err.toString()});
