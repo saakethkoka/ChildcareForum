2987d988003bb6dec46a2700aae5ce9b63b66e5e
swward <swward@smu.edu>
2022-04-21 17:31:45 -0500

    added requestRoutes and API outlines

---
+++ backend/requestRoutes.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const router = express.Router();
+const app = express();
+const connectToDatabase = require('./database-helpers');
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+router.get('/pending', async (req, res, next) => {
+    try {
+        console.log('Initiating GET /requests/pending request');
+    } catch (err) {
+        console.error('Issue retrieving pending requests', err);
+        res.status(500).json({message: err.toString()});
+    }
+
+    next();
+})
+
+router.put('/approve/:userID', async (req, res, next) => {
+    try {
+        console.log('Initiating PUT /requests/approve/[userID] request');
+    } catch (err) {
+        console.error('Issue approving verification request', err);
+        res.status(500).json({message: err.toString()});
+    }
+
+    next();
+})
+
+router.put('/reject/:userID', async (req, res, next) => {
+    try {
+        console.log('Initiating PUT /requests/reject/[userID] request');
+    } catch (err) {
+        console.error('Issue rejecting verification request', err);
+        res.status(500).json({message: err.toString()});
+    }
+
+    next();
+})
+
+router.put('/newpending', async (req, res, next) => {
+    try {
+        console.log('Initiating PUT /requests/newpending request');
+    } catch (err) {
+        console.error('Issue adding user to pending requests', err);
+        res.status(500).json({message: err.toString()});
+    }
+
+    next();
+})
+
+router.put('/removepending', async (req, res, next) => {
+    try {
+        console.log('Initiating PUT /requests/removepending request');
+    } catch (err) {
+        console.error('Issue removing user pending request', err);
+        res.status(500).json({message: err.toString()});
+    }
+
+    next();
+})
