271606a931e192df8eab026dbb216d1f6ca3733e
nsood1 <nbsood@mail.smu.edu>
2022-04-23 20:43:43 -0500

    Update ServiceRoutes.js

--- backend/ServiceRoutes.js
+++ backend/ServiceRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const router = express.Router();
 const app = express();
 const port = 3000;
 const connectToDatabase = require('./database-helpers');
@@ -134,6 +135,177 @@ module.exports = function ServiceRoutes(app, logger){
         }
     });
 
+    app.get('/newSearchPosts', async (req, res, next) => {
+        try {
+            console.log('Initiating GET /newSearchPosts request');
+            const {DBQuery, disconnect} = await connectToDatabase();
+            const searchWord = req.query.searchWord;
+
+            if(searchWord){
+                const results = await DBQuery('SELECT * from discussionBoard WHERE postEntry LIKE ' + '\'% ' + searchWord + ' %\'');
+                const postRaw = JSON.parse(JSON.stringify(results));
+
+                let formattedPosts = [];
+                for (const row in postRaw) {
+                    const statusQuery = 'SELECT isVerified, isBanned FROM statusTable WHERE userID = ' + postRaw[row].f_userID;
+                    const statusResults = await DBQuery(statusQuery);
+                    const statusRaw = JSON.parse(JSON.stringify(statusResults));
+        
+                    const userQuery = 'SELECT username FROM userLogin WHERE userID = ' + postRaw[row].f_userID;
+                    const userResults = await DBQuery(userQuery);
+                    const userRaw = JSON.parse(JSON.stringify(userResults));
+        
+                    let userVote;
+                    if (typeof req.query.curruserID == 'undefined')
+                        userVote = 0;
+                    else {
+                        const voteValueRaw = await DBQuery('SELECT value FROM postVotes WHERE f_postID = ' + postRaw[row].postID + ' AND f_userID = ' + req.query.curruserID);
+                        const voteValueObj = JSON.parse(JSON.stringify(voteValueRaw));
+                        if (typeof voteValueObj[0] == 'undefined')
+                            userVote = 0;
+                        else
+                            userVote = voteValueObj[0].value;
+                    }
+        
+                    let votes;
+                    const voteCountRaw = await DBQuery('SELECT SUM(value) AS total FROM postVotes WHERE f_postID = ' + postRaw[row].postID);
+                    const voteCountObj = JSON.parse(JSON.stringify(voteCountRaw))[0];
+                    if (voteCountObj.total === null)
+                        votes = 0;
+                    else
+                        votes = voteCountObj.total;
+        
+                    let verified;
+                    if(statusRaw[0]){
+                        verified = (statusRaw[0].isVerified !== 0)
+                    }
+                    else{
+                        verified = false;
+                    }
+        
+                    let restricted;
+                    if(postRaw[row]){
+                        console.log(postRaw[row]);
+                        restricted = (postRaw[row].isRestricted !== 0)
+                    }
+                    else{
+                        restricted = false;
+                    }
+        
+                    let userBanned;
+                    if(statusRaw[0]){
+                        userBanned = (statusRaw[0].isBanned !== 0)
+                    }
+                    else{
+                        userBanned = false;
+                    }
+        
+                    formattedPosts.push({
+                        postTitle: postRaw[row].postTitle,
+                        postID: postRaw[row].postID,
+                        userID: postRaw[row].f_userID,
+                        votes: votes,
+                        userVote: userVote,
+                        verified,
+                        date: postRaw[row].date,
+                        restricted: restricted,
+                        username: userRaw[0].username,
+                        userBanned: userBanned,
+                        postEntry: postRaw[row].postEntry
+                    });
+                }
+        
+                disconnect();
+                res.json(formattedPosts);
+            }
+
+            else{
+                const results = await DBQuery('SELECT * from discussionBoard');
+                const postRaw = JSON.parse(JSON.stringify(results));
+
+                let formattedPosts = [];
+                for (const row in postRaw) {
+                    const statusQuery = 'SELECT isVerified, isBanned FROM statusTable WHERE userID = ' + postRaw[row].f_userID;
+                    const statusResults = await DBQuery(statusQuery);
+                    const statusRaw = JSON.parse(JSON.stringify(statusResults));
+        
+                    const userQuery = 'SELECT username FROM userLogin WHERE userID = ' + postRaw[row].f_userID;
+                    const userResults = await DBQuery(userQuery);
+                    const userRaw = JSON.parse(JSON.stringify(userResults));
+        
+                    let userVote;
+                    if (typeof req.query.curruserID == 'undefined')
+                        userVote = 0;
+                    else {
+                        const voteValueRaw = await DBQuery('SELECT value FROM postVotes WHERE f_postID = ' + postRaw[row].postID + ' AND f_userID = ' + req.query.curruserID);
+                        const voteValueObj = JSON.parse(JSON.stringify(voteValueRaw));
+                        if (typeof voteValueObj[0] == 'undefined')
+                            userVote = 0;
+                        else
+                            userVote = voteValueObj[0].value;
+                    }
+        
+                    let votes;
+                    const voteCountRaw = await DBQuery('SELECT SUM(value) AS total FROM postVotes WHERE f_postID = ' + postRaw[row].postID);
+                    const voteCountObj = JSON.parse(JSON.stringify(voteCountRaw))[0];
+                    if (voteCountObj.total === null)
+                        votes = 0;
+                    else
+                        votes = voteCountObj.total;
+        
+                    let verified;
+                    if(statusRaw[0]){
+                        verified = (statusRaw[0].isVerified !== 0)
+                    }
+                    else{
+                        verified = false;
+                    }
+        
+                    let restricted;
+                    if(postRaw[row]){
+                        console.log(postRaw[row]);
+                        restricted = (postRaw[row].isRestricted !== 0)
+                    }
+                    else{
+                        restricted = false;
+                    }
+        
+                    let userBanned;
+                    if(statusRaw[0]){
+                        userBanned = (statusRaw[0].isBanned !== 0)
+                    }
+                    else{
+                        userBanned = false;
+                    }
+        
+                    formattedPosts.push({
+                        postTitle: postRaw[row].postTitle,
+                        postID: postRaw[row].postID,
+                        userID: postRaw[row].f_userID,
+                        votes: votes,
+                        userVote: userVote,
+                        verified,
+                        date: postRaw[row].date,
+                        restricted: restricted,
+                        username: userRaw[0].username,
+                        userBanned: userBanned,
+                        postEntry: postRaw[row].postEntry
+                    });
+                }
+        
+                disconnect();
+                res.json(formattedPosts);
+            }
+           
+        } catch (err) {
+            console.error('Problem retrieving discussion board posts', err);
+            res.status(500).json({message: err.toString()});
+        }
+    
+        next();
+    })
+
+
     app.get('/verifiedpostusers', async (request, response) => {
         try {
             console.log('Initiating GET /verifiedpostusers request');
@@ -159,6 +331,5 @@ module.exports = function ServiceRoutes(app, logger){
             response.status(500).json({message: err.message});
         }
     });
-
 
 }
